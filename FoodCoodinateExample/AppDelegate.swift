0031c6c6f2f89257017709d93a3dafa58ca25f51
Fumiya Sakai <just1factory@gmail.com>
2022-05-30 17:51:33 +0900

    Initial Commit

---
+++ FoodCoodinateExample/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  FoodCoodinateExample
+//
+//  Created by 酒井文也 on 2022/05/30.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ FoodCoodinateExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FoodCoodinateExample
+//
+//  Created by 酒井文也 on 2022/05/30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
